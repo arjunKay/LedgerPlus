e70d62ad440f4356fdab8e90885101776c6e073b
Haris <1995mhdharis@gmail.com>
2016-11-12 21:41:40 +0530

    Added Navigation Drawer[Need some changes later]

--- app/src/main/java/com/svco/ledgerplus/Home.java
+++ app/src/main/java/com/svco/ledgerplus/Home.java
@@ -1,10 +1,19 @@
-//Haris 9:20 build gradilil drawer library ittind..
+//Haris 9:40 drawer ittind..some changes have to be done
 
 
 package com.svco.ledgerplus;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
 
 public class Home extends AppCompatActivity {
 
@@ -13,6 +22,27 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         //this is a comment
+        AccountHeader header=new AccountHeaderBuilder()
+                .withActivity(this)
+                .withTextColor(Color.parseColor("#FF0000"))
+                //.addProfiles(new ProfileDrawerItem().withName("USER").withEmail("email id"))
+                .build();
 
+        Drawer result = new DrawerBuilder()
+                .withActivity(this)
+                .withAccountHeader(header)
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName("Graph Statistics"),
+                        new DividerDrawerItem(),
+                        new PrimaryDrawerItem().withName("Balance Sheet"),
+                        new DividerDrawerItem(),
+                        new PrimaryDrawerItem().withName("Categories"),
+                        new DividerDrawerItem(),
+                        new PrimaryDrawerItem().withName("Reminder"),
+                        new DividerDrawerItem(),
+                        new PrimaryDrawerItem().withName("Settings")
+
+        )
+                .build();
     }
 }
