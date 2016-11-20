d06c2b4db5ee80f259172c9bd893edc89d3a3e9d
Sachin <sachin.krishnan57@gmail.com>
2016-11-20 22:06:59 +0530

    PieDiagram for expense and income created in Graphstats.java

--- app/src/main/java/com/svco/ledgerplus/GraphStats.java
+++ app/src/main/java/com/svco/ledgerplus/GraphStats.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Legend;
@@ -17,6 +20,10 @@ import com.github.mikephil.charting.utils.PercentFormatter;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.svco.ledgerplus.LedgerDBManager.AMOUNT;
+import static com.svco.ledgerplus.LedgerDBManager.CATEGORY;
+import static com.svco.ledgerplus.LedgerDBManager.TABLE_TRANSACTIONS;
 
 public class GraphStats extends AppCompatActivity {
 
@@ -31,6 +38,15 @@ public class GraphStats extends AppCompatActivity {
         toolbar.setTitle("Graph Statistics");
 
         myDB=new LedgerDBManager(this);
+
+        List<String> SpinnerArray1 = new ArrayList<String>();
+        SpinnerArray1.add("Expense");
+        SpinnerArray1.add("Income");
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(GraphStats.this,android.R.layout.simple_spinner_item, SpinnerArray1);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner spinner_type = (Spinner)findViewById(R.id.spinner);
+        spinner_type.setAdapter(adapter);
+
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -51,26 +67,7 @@ public class GraphStats extends AppCompatActivity {
         Legend l=mchart.getLegend();
         l.setEnabled(false);
 
-        final List<String> discat = new ArrayList<>();  //Populate this list with Distinct Categories
-        ArrayList<Entry> entries = new ArrayList<Entry>(); //Populate this list with sum of transactions in corresponding category
-
-        //To be Edited from here ---->
-
-
-        Cursor inCur=myDB.getExCategory(); //Cursor inCur=myDB.getAllData(myDB.TABLE_TRANSACTIONS);
-        while (inCur.moveToNext()){
-            discat.add(inCur.getString(1));
-            entries.add(new Entry((float)Math.random(),10));}//entries.add(new Entry((float)inCur.getFloat(1),10));
-
-
-        //------>upto here
-
-        PieDataSet dataSet=new PieDataSet(entries,"");
-        dataSet.setSliceSpace(2f);
-
-
-
-        ArrayList<Integer> colors = new ArrayList<Integer>();
+        final ArrayList<Integer> colors = new ArrayList<Integer>();
         for (int c : ColorTemplate.VORDIPLOM_COLORS)
             colors.add(c);
         for (int c : ColorTemplate.JOYFUL_COLORS)
@@ -81,16 +78,70 @@ public class GraphStats extends AppCompatActivity {
             colors.add(c);
         for (int c : ColorTemplate.PASTEL_COLORS)
             colors.add(c);
-
         colors.add(ColorTemplate.getHoloBlue());
 
-        dataSet.setColors(colors);
 
-        PieData data=new PieData(discat,dataSet);
-        data.setValueFormatter(new PercentFormatter());
 
-        mchart.setData(data);
 
+
+        spinner_type.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                final List<String> discat_in = new ArrayList<>();  //Populate this list with Distinct Categories
+                final ArrayList<Entry> entries_in = new ArrayList<Entry>();
+
+                if(position==1){
+                    mchart.requestLayout();
+                    mchart.setCenterText("Income");
+                    String relOp=">";
+                    Cursor inCur=myDB.executeQuery("SELECT *,SUM("+AMOUNT+") FROM "+ TABLE_TRANSACTIONS + " WHERE "+AMOUNT+" "+relOp+" 0 GROUP BY "+CATEGORY);
+                    while (inCur.moveToNext()){
+                        discat_in.add(inCur.getString(3));
+                        entries_in.add(new Entry(((float)inCur.getInt(8)*100)/myDB.sumOfTxn("in"),10));}
+
+                    PieDataSet dataSet=new PieDataSet(entries_in,"");
+                    dataSet.setSliceSpace(2f);
+
+                    dataSet.setColors(colors);
+
+                    PieData data=new PieData(discat_in,dataSet);
+                    data.setValueFormatter(new PercentFormatter());
+
+                    mchart.setData(data);
+
+
+
+                }
+                else if(position==0)
+                {
+                    mchart.requestLayout();
+                    mchart.setCenterText("Expense");
+                    final List<String> discat_ex = new ArrayList<>();  //Populate this list with Distinct Categories
+                    final ArrayList<Entry> entries_ex = new ArrayList<Entry>();
+                    String relOp="<";
+                    Cursor inCur=myDB.executeQuery("SELECT *,SUM("+AMOUNT+") FROM "+ TABLE_TRANSACTIONS + " WHERE "+AMOUNT+" "+relOp+" 0 GROUP BY "+CATEGORY);
+                    while (inCur.moveToNext()){
+                        discat_ex.add(inCur.getString(3));
+                        entries_ex.add(new Entry((((float)inCur.getInt(8)*-1)*100)/myDB.sumOfTxn("ex"),10));}
+
+                    PieDataSet dataSet=new PieDataSet(entries_ex,"");
+                    dataSet.setSliceSpace(2f);
+
+                    dataSet.setColors(colors);
+
+                    PieData data=new PieData(discat_ex,dataSet);
+                    data.setValueFormatter(new PercentFormatter());
+
+                    mchart.setData(data);
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
     }
 }
