29acfcc0b5533646a9593f2df23082cb07475f90
Nidhin <nidhink1995@yahoo.in>
2016-11-28 18:42:08 +0530

    Settings activity populated with options to Reset data and edit profile(functionality to be added)

--- app/src/main/java/com/svco/ledgerplus/Settings.java
+++ app/src/main/java/com/svco/ledgerplus/Settings.java
@@ -4,10 +4,21 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Settings extends AppCompatActivity {
 
     Toolbar toolbar;
+    ListView list;
+    List<String> titleList = new ArrayList<>();
+    List<String> detailList = new ArrayList<>();
+    String[] titles= new String[]{"Reset Data","Edit Profile","About us"};
+    String[] details = new String[]{"Clear database and remove all transactions","Edit Name and e-mail id",""};
+    Integer[] image_id= {R.drawable.ic_reset,R.drawable.ic_profile,R.drawable.ic_about};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +33,12 @@ public class Settings extends AppCompatActivity {
                 finish();
             }
         });
+        titleList = new ArrayList<>(Arrays.asList(titles));
+        detailList = new ArrayList<>(Arrays.asList(details));
 
+        SettingsAdapter adapter = new SettingsAdapter(Settings.this,R.id.activity_settings,titleList,detailList,image_id);
+        list=(ListView)findViewById(R.id.list_settings);
+        list.setAdapter(adapter);
     }
+
 }
